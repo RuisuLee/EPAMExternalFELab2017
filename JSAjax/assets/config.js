492fe33fe1e91f51938675114d209f189fd5eaf1
RuisuLee <ruisu.lee@gmail.com>
2018-03-21 23:28:11 +0400

    added JSAjax task with openweathermap API

---
+++ JSAjax/assets/config.js
@@ -0,0 +1,4 @@
+const API_KEY = "26f7524f7ce5dbc3db7bd0a33f0dfd12";
+const API_URL = "http://api.openweathermap.org/data/2.5/";
+const WEATHER_FOR_WEEK_API = `${API_URL}/forecast?APPID=${API_KEY}`;
+const CURRENT_API_URL = `${API_URL}/weather?APPID=${API_KEY}`;
